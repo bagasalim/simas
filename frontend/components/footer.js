b4d938d9a404faf7f04b784f9e5af134a7c4a788
bagasalim <bagasalim10@gmail.com>
2022-10-04 10:39:50 +0700

    edit footer

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="container">
-      <div className="text-center p-2" style={{ backgroundColor: "#7E8A97", width: "inherit" }} height={82}>
+      <div className="text-center p-2" style={{ backgroundColor: "#7E8A97", width: "100%", position: "relative" }} height={82}>
         © 2022 Simas Contact dan Info
       </div>
     </div>
