4f3af4aa0f746494083cf3b9ec55668f84aa7f7f
Afif Fatur Rahman <afiffatur525@gmail.com>
2022-10-05 09:13:47 +0700

    fixing import logo error

--- frontend/pages/project/admin/index.js
+++ frontend/pages/project/admin/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../../../styles/Home.module.css";
+import logo from "../../../public/logo.png";
 import Sidebar from "../../../components/sidebaradmin/sidebarn";
 import style from "./index.module.scss";
 import HalamanUtama from "../../../components/halamanutamaadmin/halamanutama";
