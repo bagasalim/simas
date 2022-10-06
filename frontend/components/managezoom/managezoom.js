a2d190a1970d9471f5e010eecb1533cdb0b82f25
Afif Fatur Rahman <afiffatur525@gmail.com>
2022-10-06 16:59:49 +0700

    add front manage zoom and confirmation modal [red]

--- frontend/components/managezoom/managezoom.js
+++ frontend/components/managezoom/managezoom.js
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import React from "react";
 import style from "./managezoom.module.scss";
-import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+import ConfirmationModal from "../modals/modalwadanzoom/modalwadanzoom";
 
 const ManageZoom = () => {
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const [body, setBodyData] = React.useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const dataform = {
+      newlink: formData.get("newlink"),
+    };
+    body = setBodyData(dataform);
+    modalOpen = setModalOpen(true);
+  };
 
   return (
     <div className={style.zoom}>
       <h1>Manage link zoom</h1>
       <div className={style.inputbox}>
-        <form>
+        <form onSubmit={onSubmit}>
           <div>
             <h3>Link ZOOM Lama</h3>
             <input
@@ -22,12 +33,20 @@ const ManageZoom = () => {
           <br />
           <div>
             <h3>Link ZOOM Baru</h3>
-            <input type="text" />
+            <input type="text" name="newlink" required />
           </div>
           <br />
           <br />
+          <button className={style.buttonHijau}>SIMPAN</button>
         </form>
       </div>
+      <ConfirmationModal
+        show={modalOpen}
+        close={() => setModalOpen(false)}
+        linktype={"Zoom"}
+        data={body}
+      />
+      ;
     </div>
   );
 };
--- frontend/pages/testing/loginForm.test.js
+++ frontend/pages/testing/loginForm.test.js
@@ -1,3 +1,2 @@
-import Home from "../../loginForm.js";
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
