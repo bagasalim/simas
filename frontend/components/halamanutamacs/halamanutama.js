9d31f0941a0bff65f6b4a11d70d686b66c2dc297
rialfu <remasertu@gmail.com>
2022-10-06 14:58:31 +0700

    update for vercel v3.2

--- frontend/components/halamanutamacs/halamanutama.js
+++ frontend/components/halamanutamacs/halamanutama.js
@@ -16,13 +16,13 @@ const HalamanUtama = () => {
       <br />
       <div className={style.informasi}>
         <h3 style={{ fontSize: "24px", fontWeight: "450" }}>Informasi</h3>
-        <Image src={foto1} width={700} height={325} />
+        <Image src={foto1} width={700} height={325} alt="foto1" />
         <p style={{ fontSize: "20px", textAlign: "justify" }}>
           Waspada Penipuan, Begini Tips Transaksi Aman di ATM Bank Sinarmas
         </p>
         <br />
         <br />
-        <Image src={foto2} width={700} height={325} />
+        <Image src={foto2} width={700} height={325} alt="foto2"/>
         <p style={{ fontSize: "20px", textAlign: "justify" }}>
           Amankan Kartu Kredit dengan Cara Freeze Lewat Aplikasi Simobi+
         </p>
