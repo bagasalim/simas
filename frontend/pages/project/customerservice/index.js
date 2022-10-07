5d9bf997399968bbe6d86b2acc123085c5b16476
Afif Fatur Rahman <afiffatur525@gmail.com>
2022-10-07 09:56:54 +0700

    [red] fix merging

--- frontend/pages/project/customerservice/index.js
+++ frontend/pages/project/customerservice/index.js
@@ -5,53 +5,45 @@ import logo from "../../../public/logo.png";
 import Sidebar from "../../../components/sidebarcs/sidebarn";
 import style from "./index.module.scss";
 import HalamanUtama from "../../../components/halamanutamacs/halamanutama";
-<<<<<<< HEAD
-import ManageZoom from "../../../components/managezoom/managezoom";
-=======
 import { useEffect, useState } from "react";
-import Router from 'next/router';
->>>>>>> 78c2b46411decdefcab00398b889b640b7edaa23
+import Router from "next/router";
 
-export default function Index(){
-  const [loading, setLoading] = useState(true)
-  useEffect(()=>{
-    const token = localStorage.getItem("token")
-    let user = localStorage.getItem("user")
-    if(token == null || user == null ){
-      console.log("logout")
-      Router.replace('/loginForm');
-      return
+export default function Index() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    let user = localStorage.getItem("user");
+    if (token == null || user == null) {
+      console.log("logout");
+      Router.replace("/loginForm");
+      return;
     }
-    user = JSON.parse(user)
-    if(user.role != 2 ){
-      if(user.role == 1){
-        console.log("redirect")
-        Router.replace('/project/admin');
-        return
+    user = JSON.parse(user);
+    if (user.role != 2) {
+      if (user.role == 1) {
+        console.log("redirect");
+        Router.replace("/project/admin");
+        return;
       }
-      console.log("load", user)
-      Router.replace('/loginForm');
-      return
+      console.log("load", user);
+      Router.replace("/loginForm");
+      return;
     }
-    setLoading(false)
-    console.log(token, user)
-  },[])
+    setLoading(false);
+    console.log(token, user);
+  }, []);
   return (
     <div className={style.home}>
       <Sidebar />
       <div className={style.homeContainer}>
         <div className={style.content}>
-<<<<<<< HEAD
-          <ManageZoom />
-=======
-        {
-            loading ?
-              (<div>
-                <h1>Please wait</h1>
-              </div>)
-              :<HalamanUtama />
-          }
->>>>>>> 78c2b46411decdefcab00398b889b640b7edaa23
+          {loading ? (
+            <div>
+              <h1>Please wait</h1>
+            </div>
+          ) : (
+            <HalamanUtama />
+          )}
         </div>
       </div>
     </div>
