f908c4964a81d592eb65fc308f3dc07db3b31ebc
cindysurjawann <csurjawan@gmail.com>
2022-10-10 13:35:02 +0700

    [GREEN] add fungsi test service updatelink dan add go yml

--- backend/managelink/service_test.go
+++ backend/managelink/service_test.go
@@ -39,37 +39,41 @@ func TestGetLinkService(t *testing.T) {
 func TestUpdateLinkService(t *testing.T) {
 	db := newTestDB(t)
 	repo := NewRepository(db)
+	service := NewService(repo)
 
 	//Link WA
-	link := model.Link{
+	link := UpdateLinkRequest{
 		LinkType:  "WA",
 		LinkValue: "Ini Link WA Update",
 		UpdatedBy: "System",
 	}
-	res, err := repo.UpdateLink(link)
+	res, status, err := service.UpdateLink(link)
 	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, status)
 	assert.NotNil(t, res)
 	assert.Equal(t, res.LinkValue, "Ini Link WA Update")
 
 	//Link Zoom
-	link = model.Link{
+	link = UpdateLinkRequest{
 		LinkType:  "Zoom",
 		LinkValue: "Ini Link Zoom Update",
 		UpdatedBy: "System",
 	}
-	res, err = repo.UpdateLink(link)
+	res, status, err = service.UpdateLink(link)
 	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, status)
 	assert.NotNil(t, res)
 	assert.Equal(t, res.LinkValue, "Ini Link Zoom Update")
 
 	//No Link
-	link = model.Link{
+	link = UpdateLinkRequest{
 		LinkType:  "No Link",
 		LinkValue: "No Link",
 		UpdatedBy: "System",
 	}
-	res, err = repo.UpdateLink(link)
-	assert.Equal(t, err.Error(), errors.New("wrong link type").Error())
+	res, status, err = service.UpdateLink(link)
+	assert.Equal(t, err.Error(), errors.New("link not found").Error())
+	assert.Equal(t, http.StatusInternalServerError, status)
 	assert.Equal(t, res, model.Link{})
 
 }
