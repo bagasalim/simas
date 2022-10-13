1e7824b2d6f68eef9fc88d294641ce5b4b1080d8
Calvin Yonathan <calvinyonathan51@gmail.com>
2022-10-13 11:14:08 +0700

    Add PBI-7-ModalCart

--- frontend/pages/nasabah/asuransi.js
+++ frontend/pages/nasabah/asuransi.js
@@ -3,12 +3,32 @@ import UserFooter from "../../components/userfooter";
 import style from "./asuransi.module.scss";
 import Image from "next/future/image";
 import jumbotron from "../../public/jumbotron.png";
+import cslaki from "../../public/cslaki.png";
+import React from "react";
+import ConfirmationModal from "../../components/modals/modalDetailCart";
+import {useState, useEffect } from "react";
+
+
+
 const Asuransi = () => {
+  
+  const [data, setData] = useState(null);
+  const [newLink, setNewLink] = useState("");
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const [body, setBodyData] = React.useState("");
+
+const onSubmit = async (e) => {
+  const dataform = {
+    newlink: newLink,
+  };
+  setBodyData(dataform);
+  setModalOpen(true);
+};
   return (
     <div>
       <Header />
       <div>
-        <Image className={style.jumbotron} src={jumbotron} alt="jumbotron" />
+        <Image className={style.jumbotron} src={jumbotron} alt="jumbotron" hidden/>
       </div>
       <div className={style.buttonpa}>
         <div>
@@ -18,6 +38,31 @@ const Asuransi = () => {
           <button className={style.buttonasuransi}>Asuransi</button>
         </div>
       </div>
+
+      <div className={style.container}>
+      <form id="cartZoom"  onSubmit={(e) => {
+            e.preventDefault();
+          }}>
+      <div>
+        Judul
+        <input type="text" name="newlink" required value="Judul" onChange={(e) => setNewLink(e.target.value)}/>
+      </div>
+      <div>Deskripsi</div>
+      <div>
+      <Image className={style.cslaki} src={cslaki} alt="cslaki" />
+      </div>
+      <div>
+        SnK
+      </div>
+      <button onClick={onSubmit}>
+        Lihat Detail
+      </button>
+      </form>
+    
+      </div>
+      <ConfirmationModal show={modalOpen} close={() => setModalOpen(false)} />;
+
+
       <UserFooter />
     </div>
   );
