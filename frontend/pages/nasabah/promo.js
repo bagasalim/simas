1ef3a1a5be366e16363c3715afe62d7cb0aa845c
Afif Fatur Rahman <afiffatur525@gmail.com>
2022-10-17 16:38:00 +0700

    [green] add logic to connect frontend and backend for info promo

--- frontend/pages/nasabah/promo.js
+++ frontend/pages/nasabah/promo.js
@@ -11,80 +11,100 @@ import { useRouter } from "next/router";
 
 const Promo = () => {
   const [data, setData] = useState(null);
-  const [newLink, setNewLink] = useState("");
   const [modalOpen, setModalOpen] = React.useState(false);
   const [body, setBodyData] = React.useState("");
   const router = useRouter();
-
-  const onSubmit = async (e) => {
-    const dataform = {
-      newlink: newLink,
-    };
-    setBodyData(dataform);
-    setModalOpen(true);
-  };
 
   const asuransi = () => {
     router.push("/nasabah/asuransi");
   };
 
   useEffect(() => {
-    getData()
+    getData();
   }, []);
 
   const getData = async () => {
     try {
-      // const res = await fetch(`${process.env.NEXT_PUBLIC_URL}`);
-      // const data = await res.json();
-      const data = [
-        { judul: "Serunya Oktober", startdate : "1 Oktober 2022",enddate: "31 Oktober 2022", kodepromo: "S1234" },
-        { judul: "Serunya Oktober", startdate : "1 Oktober 2022",enddate: "31 Oktober 2022", kodepromo: "S1234" },
-        { judul: "Serunya Oktober", startdate : "1 Oktober 2022",enddate: "31 Oktober 2022", kodepromo: "S1234" },
-        { judul: "Serunya Oktober", startdate : "1 Oktober 2022",enddate: "31 Oktober 2022", kodepromo: "S1234" },
-        { judul: "Serunya Oktober", startdate : "1 Oktober 2022",enddate: "31 Oktober 2022", kodepromo: "S1234" }
-      ];
-      setData(data);
-    }
-    catch (e) {
-      if (typeof e === "string") {
-        alert("Gagal load data");
+      const res = await fetch(`${process.env.NEXT_PUBLIC_URL}getrecentpromos`);
+      if (res.status != 200) {
+        throw "gagal mendapatkan pesan"();
       }
+      const data = await res.json();
+      if (!data.data) {
+        throw "gagal mendapatkan data"();
+      }
+      setData(data);
+    } catch (e) {
+      alert("Gagal mengambil data");
     }
-  }
+  };
 
   return (
     <div>
       <Header />
       <div>
-        <Image className={style.jumbotron} src={jumbotron} alt="jumbotron" hidden/>
+        <Image className={style.jumbotron} src={jumbotron} alt="jumbotron" />
       </div>
       <div className={style.buttonpa}>
         <div>
           <button className={style.buttonpromoActive}>Promo</button>
         </div>
         <div>
-          <button className={style.buttonasuransi} onClick={asuransi}>Asuransi</button>
+          <button className={style.buttonasuransi} onClick={asuransi}>
+            Asuransi
+          </button>
         </div>
       </div>
-      
       {getData}
-        < div className="row justify-content-start" style={{paddingLeft:80, paddingRight:80}}>
-        {data?.map((item, index) => (
-        <div key={index} className="col-4" style={{paddingLeft:50, paddingRight:50}}>
-          <div className={style.detailContent}>
-              <Image className={style.imageDummy} src={promotionDummy} alt="promotionDummy" />
-              <h2 className={style.textContent}>{item.judul}</h2>
-              <h5 className={style.textContent}>Periode: {item.startdate} - {item.enddate}</h5>
-              <h5 className={style.textContent}>Kode Promo: {item.kodepromo}</h5>
-              <button className={style.buttonDetail} onClick={onSubmit}>Lihat Detail</button>
+      <div
+        className="row justify-content-start"
+        style={{ paddingLeft: 80, paddingRight: 80 }}
+      >
+        {data?.data?.map((item, index) => (
+          <div
+            key={index}
+            className="col-4"
+            style={{ paddingLeft: 50, paddingRight: 50 }}
+          >
+            <div className={style.detailContent}>
+              <img
+                className={style.imageDummy}
+                src={item.foto}
+                alt={item.judul}
+                width={100}
+                height={100}
+              />
+              <h3 className={style.textContent}>{item.judul}</h3>
+              <h5 className={style.textContent} style={{ fontSize: "1rem" }}>
+                Periode: {item.startdate.substring(0, 10)} s/d{" "}
+                {item.enddate.substring(0, 10)}
+              </h5>
+              <h5 className={style.textContent} style={{ fontSize: "1rem" }}>
+                Kode Promo: {item.kodepromo}
+              </h5>
+              <button
+                className={style.buttonDetail}
+                onClick={() => {
+                  setBodyData(item);
+                  setModalOpen(true);
+                }}
+              >
+                Lihat Detail
+              </button>
+            </div>
           </div>
-        </div>
         ))}
-        </div>
-
-      <ConfirmationModal show={modalOpen} close={() => setModalOpen(false)} />;
+      </div>
+      {body ? (
+        <ConfirmationModal
+          show={modalOpen}
+          close={() => setModalOpen(false)}
+          data={body}
+        />
+      ) : (
+        ""
+      )}
       <UserFooter />
-
     </div>
   );
 };
